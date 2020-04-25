aa507909967acf7ccc366f101b2f09f040c42dff
TiM <tm888@mail.ru>
2020-04-25 13:13:44 +0500

    upd 6

--- assets/src/js/my.js
+++ assets/src/js/my.js
@@ -5,6 +5,7 @@ var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 var player;
+var player2;
 
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
@@ -13,6 +14,14 @@ function onYouTubeIframeAPIReady() {
     videoId: 'JAOn10zOOak',
     events: {
       'onStateChange': onPlayerStateChange
+    }
+  });
+  player2 = new YT.Player('player2', {
+    height: '315',
+    width: '560',
+    videoId: '8PEgNTJK-TA',
+    events: {
+      'onStateChange': onPlayerStateChange2
     }
   });
 }
@@ -24,8 +33,14 @@ function onPlayerStateChange(state) {
   }
 }
 
+function onPlayerStateChange2(state) {
+  console.log(state);
+  if (state.data === 0) {
+    stopVideo2();
+  }
+}
+
 function startVideo() {
-  player.setVolume(0);
   player.playVideo();
   document.getElementById("videoCover").style.display = 'none';
   document.getElementById("videoPlayer").style.display = 'block';
@@ -34,4 +49,15 @@ function startVideo() {
 function stopVideo() {
   document.getElementById("videoPlayer").style.display = 'none';
   document.getElementById("videoCover").style.display = 'block';
+}
+
+function startVideo2() {
+  player2.playVideo();
+  document.getElementById("videoCover2").style.display = 'none';
+  document.getElementById("videoPlayer2").style.display = 'block';
+}
+
+function stopVideo2() {
+  document.getElementById("videoPlayer2").style.display = 'none';
+  document.getElementById("videoCover2").style.display = 'block';
 }
